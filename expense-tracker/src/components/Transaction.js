317b9875ffa853355d0aebea323cfcf26a65ae35
Rumman <rummanfardeen4567@gmail.com>
2023-05-02 22:38:29 +0530

    Files Added

--- expense-tracker/src/components/Transaction.js
+++ expense-tracker/src/components/Transaction.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const Transaction = ({transaction}) => {
+const Transaction = ({ transaction }) => {
+    const sign = transaction.amount < 0 ? '-' : '+';
     return (
         < li class="minus" >
-            {transaction.text} <span > -$400</span > <button class="delete-btn">x</button>
+            {transaction.text} <span > {sign}${Math.abs(transaction.amount)}</span><button class="delete-btn">x</button>
         </li >
     )
 }
