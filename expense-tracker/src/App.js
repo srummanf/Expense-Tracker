b903afda5f0df20c6bba5a7664798643b55f9384
Rumman <rummanfardeen4567@gmail.com>
2023-05-02 23:07:20 +0530

    Files Added

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -5,6 +5,13 @@ import IncomeExpenses from './components/IncomeExpenses';
 import TransactionList from './components/TransactionList';
 import AddTransaction from './components/AddTransaction';
 import { GlobalProvider } from './context/GlobalState';
+import React, { useEffect } from 'react';
+
+function Example() {
+  useEffect(() => {
+    document.title = 'My Page Title';
+  }, []);
+}
 
 function App() {
   return (
