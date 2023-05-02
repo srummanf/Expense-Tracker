d2dd57db2444ddfff1ef379b8403999d1ee54d43
Rumman <rummanfardeen4567@gmail.com>
2023-05-02 22:53:29 +0530

    Files Added

--- expense-tracker/src/components/Transaction.js
+++ expense-tracker/src/components/Transaction.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import {GlobalContext} from '../context/GlobalState';
 
 const Transaction = ({ transaction }) => {
     const sign = transaction.amount < 0 ? '-' : '+';
+    const {deleteTransaction} = useContext(GlobalContext);
     return (
         < li class={transaction.amount <0 ? 'minus': 'plus'} >
-            {transaction.text} <span > {sign}${Math.abs(transaction.amount)}</span><button class="delete-btn">x</button>
+            {transaction.text} <span > {sign}${Math.abs(transaction.amount)}</span><button class="delete-btn" onClick={()=>deleteTransaction(transaction.id)}>x</button>
         </li >
     )
 }
