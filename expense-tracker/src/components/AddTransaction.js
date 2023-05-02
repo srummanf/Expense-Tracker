d0e13352b7935d9b82ea9ee273ec294b33414c30
Rumman <rummanfardeen4567@gmail.com>
2023-05-02 23:01:42 +0530

    Files Added

--- expense-tracker/src/components/AddTransaction.js
+++ expense-tracker/src/components/AddTransaction.js
@@ -1,23 +1,37 @@
-import React, {useState} from 'react'
+import React, { useState, useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState';
 
 const AddTransaction = () => {
 
     const [text, setText] = useState('');
     const [amount, setAmount] = useState(0);
+    const { addTransaction } = useContext(GlobalContext);
+
+
+    const onSubmit = e => {
+        e.preventDefault();
+        const newTransaction = {
+            id: Math.floor(Math.random() * 100000000),
+            text,
+            amount: +amount
+        }
+        addTransaction(newTransaction);
+    }
     return (
         <>
             <h3>Add new transaction</h3>
-            <form id="form">
+            <form onSubmit={onSubmit} id="form">
                 <div className="form-control">
                     <label htmlFor="text">Text</label>
-                        <input type="text" value={text} onChange={(e)=>setText(e.target.value)} />id="text" placeholder="Enter text..." />
+                    <input type="text" value={text} onChange={(e) => setText(e.target.value)} id="text" placeholder="Enter text..."></input>
                 </div>
                 <div className="htmlForm-control">
-                    <label htmlFor="amount"
-                    >Amount <br />
-                        (negative - expense, positive - income)</label
-                    >
-                    <input type="number" id="amount" value={amount} onChange={(e)=>setAmount(e.target.value)} placeholder="Enter amount..." />
+                    <label htmlFor="amount">
+                    Amount <br />
+                        (negative - expense, positive - income)
+                        </label>
+                    
+                    <input type="number" id="amount" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
                 </div>
                 <button className="btn">Add transaction</button>
             </form>
--- expense-tracker/src/components/Transaction.js
+++ expense-tracker/src/components/Transaction.js
@@ -1,12 +1,12 @@
-import React,{useContext} from 'react'
-import {GlobalContext} from '../context/GlobalState';
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState';
 
 const Transaction = ({ transaction }) => {
     const sign = transaction.amount < 0 ? '-' : '+';
-    const {deleteTransaction} = useContext(GlobalContext);
+    const { deleteTransaction } = useContext(GlobalContext);
     return (
-        < li class={transaction.amount <0 ? 'minus': 'plus'} >
-            {transaction.text} <span > {sign}${Math.abs(transaction.amount)}</span><button class="delete-btn" onClick={()=>deleteTransaction(transaction.id)}>x</button>
+        < li class={transaction.amount < 0 ? 'minus' : 'plus'} >
+            {transaction.text} <span > {sign}${Math.abs(transaction.amount)}</span><button class="delete-btn" onClick={() => deleteTransaction(transaction.id)}>x</button>
         </li >
     )
 }
